1368999fb84c6efc9f662208c6cffce2f9531e66
minstargram <165654019+minstargram@users.noreply.github.com>
2024-10-10 13:08:48 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+      password  = prompt("비밀번호");
+    if(password == 1111){
+        document.write('로그인에 성공하셨습니다');
+     } else{
+        document.write("id또는 password가 틀립니다");
+    }
+    </script>
+</body>
+</html>
